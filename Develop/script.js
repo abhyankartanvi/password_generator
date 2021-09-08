82a90edb44ed136f7dd908f52323b2c01f5c8b98
abhyankartanvi <abhyankar.tanvi1993@gmail.com>
2021-09-08 17:14:05 +1000

    final commit

--- Develop/script.js
+++ Develop/script.js
@@ -5,6 +5,7 @@ var confirmNumbers;
 var confirmLowercase;
 var confirmUppercase;
 var confirmSpecialCharacters;
+var characterType;
 
 //various arrays
 var specialCharacters = [
@@ -94,8 +95,8 @@ var upperCase = [
 var generateBtn = document.querySelector("#generate");
 
 //Prompt to confirm how many characters the user would like in their password
-function generatePassword() {
-  var confirmCharacters = prompt(
+function generateArray() {
+  confirmCharacters = prompt(
     "How many characters would you like in your password? Please choose between 8 and 128"
   );
   //if statement for character confirmation
@@ -122,7 +123,7 @@ function generatePassword() {
     !confirmUppercase &&
     !confirmSpecialCharacters
   ) {
-    alert("Please choose atleast one character type");
+    characterType = alert("Please choose atleast one character type");
   }
   //if statement for the condition = user selects all 4 character types
   else if (
@@ -131,54 +132,55 @@ function generatePassword() {
     confirmUppercase === true &&
     confirmSpecialCharacters === true
   ) {
-    return numbers + lowerCase + upperCase + specialCharacters;
+    return (characterType =
+      numbers + lowerCase + upperCase + specialCharacters);
   }
   //else if statement for the condition = user selects 3 character types
-  else if (
-    confirmNumbers === true &&
-    confirmLowercase === true &&
-    confirmUppercase === true
-  ) {
-    return numbers + lowerCase + upperCase;
-  } else if (
-    confirmNumbers === true &&
-    confirmLowercase === true &&
-    confirmSpecialCharacters === true
-  ) {
-    return numbers + lowerCase + specialCharacters;
-  } else if (
-    confirmNumbers === true &&
-    confirmUppercase === true &&
-    confirmSpecialCharacters === true
-  ) {
-    return numbers + upperCase + specialCharacters;
+  else if (confirmNumbers && confirmLowercase && confirmUppercase) {
+    return (characterType = numbers + lowerCase + upperCase);
+  } else if (confirmNumbers && confirmLowercase && confirmSpecialCharacters) {
+    return (characterType = numbers + lowerCase + specialCharacters);
+  } else if (confirmNumbers && confirmUppercase && confirmSpecialCharacters) {
+    return (characterType = numbers + upperCase + specialCharacters);
   } else if (confirmLowercase && confirmUppercase && confirmSpecialCharacters) {
-    return lowerCase + upperCase + specialCharacters;
+    return (characterType = lowerCase + upperCase + specialCharacters);
   }
   //else if statement for the condition = user selects 2 character type (6 poss)
   else if (confirmNumbers && confirmLowercase) {
-    return numbers + lowerCase;
+    return (characterType = numbers + lowerCase);
   } else if (confirmNumbers && confirmUppercase) {
-    return numbers + upperCase;
+    return (characterType = numbers + upperCase);
   } else if (confirmNumbers && confirmSpecialCharacters) {
-    return numbers + specialCharacters;
+    return (characterType = numbers + specialCharacters);
   } else if (confirmLowercase && confirmUppercase) {
-    return lowerCase + upperCase;
+    return (characterType = lowerCase + upperCase);
   } else if (confirmLowercase && confirmSpecialCharacters) {
-    return lowerCase + specialCharacters;
+    return (characterType = lowerCase + specialCharacters);
   } else if (confirmUppercase + confirmSpecialCharacters) {
-    return upperCase + specialCharacters;
+    return (characterType = upperCase + specialCharacters);
   }
   //else if statement for the condition = user selects 1 character type (4 poss)
   else if (confirmNumbers) {
-    return numbers;
+    return (characterType = numbers);
   } else if (confirmLowercase) {
-    return lowerCase;
+    return (characterType = lowerCase);
   } else if (confirmUppercase) {
-    return upperCase;
+    return (characterType = upperCase);
   } else if (confirmSpecialCharacters) {
-    return specialCharacters;
+    return (characterType = specialCharacters);
   }
+}
+
+//Math function created to generate random password. Assisted by TA
+function generatePassword() {
+  var password = "";
+  var passwordArray = generateArray();
+  var characterLength = parseInt(confirmCharacters);
+  for (var i = 0; i < characterLength; i++) {
+    var randChar = Math.floor(Math.random() * characterLength);
+    password += passwordArray[randChar];
+  }
+  return password;
 }
 
 // Write password to the #password input
